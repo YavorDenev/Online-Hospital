043a2ad1e5ba646b9f58f2d662a4b6b622f3e612
Yavor <d69@abv.bg>
2023-03-05 19:11:13 +0200

    create queries

--- src/main/java/DigRz/OnlineHospital/repositories/AppointmentRepository.java
+++ src/main/java/DigRz/OnlineHospital/repositories/AppointmentRepository.java
@@ -4,6 +4,7 @@ import DigRz.OnlineHospital.entities.Appointment;
 import DigRz.OnlineHospital.entities.Doctor;
 import DigRz.OnlineHospital.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -15,6 +16,18 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
 
     List<Appointment> findByPatientAndMyDateAndMyTime(Patient patient, String myDate, String myTime);
 
+    List<Appointment> findByDoctorOrderByPatientId(Doctor doctor);
 
-    List<Appointment> findByDoctorOrderByPatientNames(Doctor doctor);
+
+
+//    List<Appointment> findByDoctorOrderByMyDateMyTime(Doctor doctor);
+
+
+
+//    List<Appointment> findByDoctorOrderByPatientNames(Doctor doctor);
+
+
+
 }
+
+
