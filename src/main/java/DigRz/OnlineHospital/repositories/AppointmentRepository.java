93e69084f5262c09ece0b0f7f2fb1c93386babf1
Yavor <d69@abv.bg>
2023-03-05 18:01:16 +0200

    create queries

--- src/main/java/DigRz/OnlineHospital/repositories/AppointmentRepository.java
+++ src/main/java/DigRz/OnlineHospital/repositories/AppointmentRepository.java
@@ -14,4 +14,7 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
     List<Appointment> findByDoctorAndMyDateAndMyTime(Doctor doctor, String myDate, String myTime);
 
     List<Appointment> findByPatientAndMyDateAndMyTime(Patient patient, String myDate, String myTime);
+
+
+    List<Appointment> findByDoctorOrderByPatientNames(Doctor doctor);
 }
